14b2381ad7d663946354a1506c58c52247d1d8a0
arminst <arminst12345@gmail.com>
2022-12-02 23:03:00 +0100

    narudzba

---
+++ GymTeam.API/GymTeam/Models/Narudzba.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GymTeam.Models
+{
+    public class Narudzba
+    {
+        [Key]
+        public int id { get; set; }
+        public int brojNarudzbe { get; set; }
+        public DateTime datumNarudzbe { get; set; }
+        public string popust { get; set; }
+        public string cijena { get; set; }
+        [ForeignKey("korisnikID")]
+        public int korisnikID { get; set; }
+        public Korisnik korisnik { get; set; }
+    }
+}
