44b6fa3f783281a161a7b56f02d4d304adf3a3a2
arminst <arminst12345@gmail.com>
2022-12-02 23:23:22 +0100

    ClanarinaPlacanje

---
+++ GymTeam.API/GymTeam/Models/ClanarinaPlacanje.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GymTeam.Models
+{
+    public class ClanarinaPlacanje
+    {
+        [ForeignKey("clanarinaID")]
+        public int clanarinaID { get; set; }
+        public Clanarina clanarina { get; set; }
+        [ForeignKey("placanjeID")]
+        public int placanjeID { get; set; }
+        public Placanje placanje { get; set; }
+    }
+}
