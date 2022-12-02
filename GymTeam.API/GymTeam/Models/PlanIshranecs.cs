339188b5008e826af0d9452890729db472aac744
arminst <arminst12345@gmail.com>
2022-12-02 17:42:23 +0100

    plan ishrane

---
+++ GymTeam.API/GymTeam/Models/PlanIshranecs.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymTeam.Models
+{
+    public class PlanIshranecs
+    {
+        [Key]
+        public int id { get; set; }
+        public string naziv { get; set; }
+        public int ukupanBrojKalorija { get; set; }
+    }
+}
