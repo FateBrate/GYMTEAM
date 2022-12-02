e464c304b2808acd99f9af7435829f95bd2103b0
arminst <arminst12345@gmail.com>
2022-12-02 23:15:51 +0100

    obavijesti

---
+++ GymTeam.API/GymTeam/Models/Obavijest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymTeam.Models
+{
+    public class Obavijest
+    {
+        [Key]
+        public int id { get; set; }
+        public string naslov { get; set; }
+        public DateTime datumObjave { get; set; }
+        public string tip { get; set; }
+        public string sadrzaj { get; set; }
+    }
+}
