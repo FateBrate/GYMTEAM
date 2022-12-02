ec768b5846bab2665ae965e8ecad7391874d5456
arminst <arminst12345@gmail.com>
2022-12-02 23:30:59 +0100

    narudzba placanje

---
+++ GymTeam.API/GymTeam/Models/NarudzbaPlacanje.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GymTeam.Models
+{
+    public class NarudzbaPlacanje
+    {
+        [ForeignKey("narudzbaID")]
+        public int narudzbaID { get; set; }
+        public Narudzba narudzba { get; set; }
+        [ForeignKey("placanjeID")]
+        public int placanjeID { get; set; }
+        public Placanje placanje { get; set; }
+    }
+}
