09e38948d541d05cf775f8732395f350ba2f3c4b
FateBrate <drek.fatih@live.com>
2022-12-12 17:45:31 +0100

    Lokacija kontroler,adresa kontroler

--- GymTeam.API/GymTeam/Controllers/AdresaControler.cs
+++ GymTeam.API/GymTeam/Controllers/AdresaControler.cs
@@ -1,5 +1,6 @@
 ﻿using GymTeam.Data;
 using GymTeam.Moduls;
+using GymTeam.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GymTeam.Controllers
@@ -17,10 +18,19 @@ namespace GymTeam.Controllers
             this._dbcontext= dbcontext;
         }
         [HttpPost]
-      
-        public IActionResult Post(Adresa adresa)
+     public Adresa Add([FromBody]AdresaAddVM x)
         {
-            return Ok(_dbcontext.Adresa.Add(adresa));
+            var adresa = new Adresa
+            {
+                NazivUlice= x.NazivUlice,
+                nazivGrada= x.nazivGrada,
+                postanskiBroj= x.postanskiBroj,
+            };
+            _dbcontext.Add(adresa);
+            _dbcontext.SaveChanges();
+            return adresa;
         }
+   
+ 
     }
 }
