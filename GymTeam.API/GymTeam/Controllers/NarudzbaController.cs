6aa98938d3cf3a84895a8e088e7cef5e29326ea1
arminst <arminst12345@gmail.com>
2022-12-13 17:45:36 +0100

    kontroleri za narudzbu i privatnog trenera

---
+++ GymTeam.API/GymTeam/Controllers/NarudzbaController.cs
@@ -0,0 +1,35 @@
+﻿using GymTeam.Data;
+using GymTeam.Models;
+using GymTeam.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymTeam.Controllers
+{
+    [Route("api/[controller]")]
+
+    [ApiController]
+
+    public class NarudzbaControler : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbcontext;
+        public NarudzbaControler(ApplicationDbContext dbcontext)
+        {
+            this._dbcontext = dbcontext;
+        }
+        [HttpPost]
+        public Narudzba Add([FromBody]NarduzbaAddVM z)
+        {
+            var narudzba = new Narudzba
+            {
+                brojNarudzbe = z.brojNarudzbe,
+                datumNarudzbe = z.datumNarudzbe,
+                popust=z.popust,
+                cijena=z.cijena,
+                korisnikID=z.korisnikID,
+            };
+            _dbcontext.Add(narudzba);
+            _dbcontext.SaveChanges();
+            return narudzba;
+        }
+    }
+}
---
+++ GymTeam.API/GymTeam/Controllers/PrivatniTrenerController.cs
@@ -0,0 +1,35 @@
+﻿using GymTeam.Data;
+using GymTeam.Models;
+using GymTeam.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymTeam.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrivatniTrenerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbcontext;
+
+        public PrivatniTrenerController(ApplicationDbContext dbContext)
+        {
+            this._dbcontext= dbContext;
+        }
+        [HttpPost]
+        public PrivatniTrener Add([FromBody]PrivatniTrenerAddVM z)
+        {
+            var privatniTrener = new PrivatniTrener
+            {
+                ime = z.ime,
+                prezime = z.prezime,
+                slika = z.slika,
+                email = z.email,
+                brojTelefona = z.brojTelefona,
+                adresa = z.adresa,
+            };
+            _dbcontext.Add(privatniTrener);
+            _dbcontext.SaveChanges();
+            return privatniTrener;
+        }
+    }
+}
