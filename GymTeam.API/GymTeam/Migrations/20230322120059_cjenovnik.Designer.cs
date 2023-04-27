4ccabf9b3cdd056d865404e1130fd7c96caa62ef
FateBrate <drek.fatih@live.com>
2023-04-28 01:41:59 +0200

    price list completed,news crud,news front,image helpers...

--- GymTeam.API/GymTeam/Migrations/20230322120059_cjenovnik.Designer.cs
+++ GymTeam.API/GymTeam/Migrations/20230322120059_cjenovnik.Designer.cs
@@ -4,6 +4,7 @@ using GymTeam.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GymTeam.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230322120059_cjenovnik")]
+    partial class cjenovnik
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -281,9 +284,6 @@ namespace GymTeam.Migrations
                     b.Property<string>("sadrzaj")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
-
-                    b.Property<byte[]>("slika")
-                        .HasColumnType("varbinary(max)");
 
                     b.Property<string>("tip")
                         .IsRequired()
