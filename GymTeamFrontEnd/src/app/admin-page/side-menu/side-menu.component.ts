1aa0122b88a88c149283102d9c52e3c4fa7f200e
FateBrate <drek.fatih@live.com>
2023-05-07 23:57:20 +0200

    data service

--- GymTeamFrontEnd/src/app/admin-page/side-menu/side-menu.component.ts
+++ GymTeamFrontEnd/src/app/admin-page/side-menu/side-menu.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { IAuth } from 'src/app/service/models/login';
 import { IUser } from 'src/app/service/models/user';
 import { CookieService } from 'ngx-cookie-service';
@@ -7,36 +13,69 @@ import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { DataService } from 'src/app/service/data.service';
 @Component({
   selector: 'app-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrls: ['./side-menu.component.css'],
-  template:
-    '`<app-admin-info (refreshUser)="loadUserData()"></app-admin-info>`',
 })
 export class SideMenuComponent implements OnInit {
   constructor(
+    private dataService: DataService,
     private cookie: CookieService,
     private router: Router,
     private httpClient: HttpClient
   ) {}
+
   korisnik: any;
 
   ngOnInit(): void {
+    // this.dataService.getUserUpdatedListener().subscribe((user: any) => {
+    //   this.korisnik = user;
+    //   console.log('user', user);
+    // });
+    // this.loadUserData();
+    this.allNighz();
+  }
+
+  allNighz() {
+    // this.dataService.getUserUpdatedListener().subscribe((user: any) => {
+    //   console.log(user, 'hahahah');
+    //   // this.korisnik = user;
+    // });
     this.loadUserData();
   }
+
   logout() {
     this.router.navigate(['login']);
     this.cookie.delete;
-    sessionStorage.clear;
+    sessionStorage.clear();
   }
-  loadUserData() {
+  // loadUserData() {
+  //   const cookieValue = this.cookie.get(COOKIE_USER_DATA);
+  //   if (cookieValue) {
+  //     let userId = JSON.parse(cookieValue);
+  //     this.httpClient
+  //       .get(`${routerpath}/api/Korisnik/GetById?id=${userId}`)
+  //       .subscribe((res) => {
+  //         if (!!res) {
+  //           this.korisnik = res;
+  //         }
+  //       });
+  //   }
+  // }
+  async loadUserData() {
+    this.dataService.userUpdated.emit();
+    console.log('ja ba');
+    this.korisnik = await this.dataService.loadUserData();
+    console.log(this.korisnik, 'halo');
+    console.log(this.korisnik, 'sidemenu loadUserData');
     const cookieValue = this.cookie.get(COOKIE_USER_DATA);
     if (cookieValue) {
       let userId = JSON.parse(cookieValue);
       this.httpClient
         .get(`${routerpath}/api/Korisnik/GetById?id=${userId}`)
-        .subscribe((res) => {
+        .subscribe((res: any) => {
           if (!!res) {
             this.korisnik = res;
           }
