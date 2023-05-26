be0da84282847a1b7d80e9a790deb65e2655b9a1
FateBrate <drek.fatih@live.com>
2023-05-26 23:57:23 +0200

    JWT setup

--- GymTeam.API/GymTeam/Controllers/AutentifikacijaController.cs
+++ GymTeam.API/GymTeam/Controllers/AutentifikacijaController.cs
@@ -5,6 +5,10 @@ using GymTeam.LoginModels;
 using Microsoft.AspNetCore.Mvc;
 using static GymTeam.Helper.AutentifikacijaAutorizacija.MyAuthTokenExtension;
 using GymTeam.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace GymTeam.Controllers
 {
@@ -24,34 +28,60 @@ namespace GymTeam.Controllers
         [HttpPost]
         public ActionResult<LoginInformacije> Login([FromBody] LoginVM x)
         {
-           
             Korisnik logiraniKorisnik = _dbContext.Korisnik
-                .FirstOrDefault(k =>
-                k.email != null && k.email == x.email && k.lozinka == x.lozinka);
+        .FirstOrDefault(k =>
+        k.email != null && k.email == x.email && k.lozinka == x.lozinka);
 
             if (logiraniKorisnik == null)
             {
-              
-                return new LoginInformacije(null);
+                return Unauthorized();
             }
 
-           
-            string randomString = TokenGenerator.Generate(10);
-
-          
-            var noviToken = new AutentifikacijaToken()
+            var claims = new[]
             {
-                ipAdresa = Request.HttpContext.Connection.RemoteIpAddress?.ToString(),
-                vrijednost = randomString,
-                korisnik = logiraniKorisnik,
-                vrijemeEvidentiranja = DateTime.Now
+        new Claim(ClaimTypes.Name, logiraniKorisnik.email),
+        
+             };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("your-secret-key");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7), // Token expiration date
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
 
-            _dbContext.Add(noviToken);
-            _dbContext.SaveChanges();
+            return Ok(tokenString);
+            //Korisnik logiraniKorisnik = _dbContext.Korisnik
+            //    .FirstOrDefault(k =>
+            //    k.email != null && k.email == x.email && k.lozinka == x.lozinka);
 
-           
-            return new LoginInformacije(noviToken);
+            //if (logiraniKorisnik == null)
+            //{
+
+            //    return new LoginInformacije(null);
+            //}
+
+
+            //string randomString = TokenGenerator.Generate(10);
+
+
+            //var noviToken = new AutentifikacijaToken()
+            //{
+            //    ipAdresa = Request.HttpContext.Connection.RemoteIpAddress?.ToString(),
+            //    vrijednost = randomString,
+            //    korisnik = logiraniKorisnik,
+            //    vrijemeEvidentiranja = DateTime.Now
+            //};
+
+            //_dbContext.Add(noviToken);
+            //_dbContext.SaveChanges();
+
+
+            //return new LoginInformacije(noviToken);
         }
 
         [HttpPost]
