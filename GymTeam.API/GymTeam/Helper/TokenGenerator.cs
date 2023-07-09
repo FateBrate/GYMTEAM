e7b17ad119939aa7f00f10430299c06ea9f8d5ba
FateBrate <drek.fatih@live.com>
2023-07-09 20:55:20 +0200

    visak

--- GymTeam.API/GymTeam/Helper/TokenGenerator.cs
+++ GymTeam.API/GymTeam/Helper/TokenGenerator.cs
@@ -25,25 +25,25 @@ namespace GymTeam.Helper
             return result.ToString();
         }
 
-        public static string GenerisiIme(int size)
-        {
-            // Characters except I, l, O, 1, and 0 to decrease confusion when hand typing tokens
-            var charSet = "ABCDEFGHJKLMNPQRSTUVWXYZ".ToLower();
-            var chars = charSet.ToCharArray();
-            var data = new byte[1];
-#pragma warning disable SYSLIB0023 // Type or member is obsolete
-            var crypto = new RNGCryptoServiceProvider();
-#pragma warning restore SYSLIB0023 // Type or member is obsolete
-            crypto.GetNonZeroBytes(data);
-            data = new byte[size];
-            crypto.GetNonZeroBytes(data);
-            var result = new StringBuilder(size);
-            foreach (var b in data)
-            {
-                result.Append(chars[b % (chars.Length)]);
-            }
-            var s = result.ToString();
-            return "S" + result;
-        }
+//        public static string GenerisiIme(int size)
+//        {
+//            // Characters except I, l, O, 1, and 0 to decrease confusion when hand typing tokens
+//            var charSet = "ABCDEFGHJKLMNPQRSTUVWXYZ".ToLower();
+//            var chars = charSet.ToCharArray();
+//            var data = new byte[1];
+//#pragma warning disable SYSLIB0023 // Type or member is obsolete
+//            var crypto = new RNGCryptoServiceProvider();
+//#pragma warning restore SYSLIB0023 // Type or member is obsolete
+//            crypto.GetNonZeroBytes(data);
+//            data = new byte[size];
+//            crypto.GetNonZeroBytes(data);
+//            var result = new StringBuilder(size);
+//            foreach (var b in data)
+//            {
+//                result.Append(chars[b % (chars.Length)]);
+//            }
+//            var s = result.ToString();
+//            return "S" + result;
+//        }
     }
 }
